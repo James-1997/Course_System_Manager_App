b3dfd546efbee7d7d9fcad31567b5e28a3498d3d
Robson James <james.robson.junior@gmail.com>
2019-11-21 01:09:02 -0400

    Created RootViewControler, Added Subview, Layout and Theme code architecture
    Signed-off-by: Robson James <james.robson.junior@gmail.com>

--- Course System Manager/Utilities/SceneDelegate.swift
+++ Course System Manager/Utilities/SceneDelegate.swift
@@ -17,7 +17,19 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let scene = (scene as? UIWindowScene) else { return }
+        let window = UIWindow(windowScene: scene)
+        //        var rootVC: UIViewController = RootViewController()
+        //        if UserDefaults().bool(forKey: .kUserIsClient) {
+        //            rootVC = MainClientViewController()
+        //        } else if UserDefaults().bool(forKey: .kUserIsProducer) {
+        //            rootVC = HomeProducerViewController()
+        //        }
+        let rootVC: UIViewController = RootViewController()
+        rootVC.modalPresentationStyle = .fullScreen
+        window.rootViewController = rootVC
+        window.makeKeyAndVisible()
+        self.window = window
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
